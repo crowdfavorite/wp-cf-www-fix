1485ee6638a2b258c71956317616f7c80f32ddfb
sparker <sparker@546ca710-db30-0410-8887-d9dd8e66aff0>
2009-04-09 17:30:03 +0000

    Adding a content filter for finding local server links that don't have www. and fixing them.
    git-svn-id: https://git.crowdfave.org/svn/crowdfavorite/code/wp-plugins/cf-www-fix/trunk@9603 546ca710-db30-0410-8887-d9dd8e66aff0

--- cf-www-fix.php
+++ cf-www-fix.php
@@ -24,7 +24,23 @@ add_filter('option_siteurl','cf_www_fix');
 add_filter('template_directory_uri','cf_www_fix');
 add_filter('stylesheet_directory_uri','cf_www_fix');
 add_filter('stylesheet_uri','cf_www_fix');
-
-// needed to catch when attachment urls are chosen based on the GUID value
 add_filter('wp_get_attachment_url','cf_www_fix');
+
+/**
+ * Catch $_SERVER['SERVER_NAME'] in the content without www. and fix it.
+ * All media gallery files inserted with this plugin enabled will have the www from the above fixes, but legacy content, 
+ * or mistakenly missing www. will not
+ *
+ * @param string $content 
+ * @return string
+ */
+function cf_www_content_fix($content) {
+	if(is_feed() || is_trackback() || is_404() || is_search() || is_tax() || is_home() || is_attachment() || is_single() || is_page() || is_category() || is_tag() || is_author() || is_date() || is_archive() || is_comments_popup() || is_paged()) {
+		$find = 'http://'.str_replace('www.','',$_SERVER['SERVER_NAME']);
+		$replace = 'http://'. (strpos($_SERVER['SERVER_NAME'],'www') !== 0 ? 'www.' : null) .$_SERVER['SERVER_NAME'];
+		$content = str_replace($find,$replace,$content);
+	}
+	return $content;
+}
+add_filter('the_content','cf_www_content_fix');
 ?>
